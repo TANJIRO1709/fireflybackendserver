5acba8aa38ab318c099e1a79e687acb52c56727c
VVUNitian <bibhupb2004@gmail.com>
2024-12-06 14:07:23 +0530

    fixed login issuess

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,156 +1,3 @@
-// const bcrypt = require("bcryptjs");
-// const User = require("../models/user");
-// const { validationResult } = require("express-validator");
-// require("dotenv").config();
-
-// exports.getLogin = (req, res, next) => {
-//   let session = req.session.id;
-//   console.log("Request session in getLogin", req.session);
-//   res.json({ success: req.session.isLoggedIn, message: session });
-// };
-
-// exports.getUser = (req, res, next) => {
-//   let session = req.session.id;
-//   console.log("Request session in getUser", req.session);
-//   if (session) {
-//     User.findById(req.session.userId)
-//       .then((user) => {
-//         if (user) {
-//           res.json({
-//             success: true,
-//             user: {
-//               userName: user.userName,
-//               email: user.email,
-//               cart: user.cart,
-//               likedProducts: user.likedProducts,
-//               models: user.models,
-//             },
-//           });
-//         } else {
-//           res.json({ success: false, message: "No user Found" });
-//         }
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         res.json({ success: false, message: err.toString() });
-//       });
-//   } else {
-//     res.json({ success: false, message: "No session found" });
-//   }
-// };
-
-// exports.postGoogleSignin = async (req, res, next) => {
-//   try {
-//     console.log("LOGGING IN TO GOOGLE");
-//     const { name: userName, email, password } = req.body;
-//     let user = await User.findOne({ email: email });
-//     if (!user) {
-//       const hashPassword = await bcrypt.hash(password, 12);
-//       user = new User({
-//         userName,
-//         email,
-//         password: hashPassword,
-//         cart: { items: [] },
-//       });
-//       await user.save();
-//       console.log({ success: true, message: "User Created" });
-//     }
-//     req.session.isLoggedIn = true;
-//     req.session.userId = user.id;
-//     console.log("SAVING SESSION");
-//     await req.session.save();
-//     console.log("Session saved successfully", req.session);
-//     res.json({ success: true, message: "Session saved successfully" });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ success: false, message: err.toString() });
-//   }
-// };
-
-// exports.postLogin = async (req, res, next) => {
-//   const { email, password } = req.body;
-//   try {
-//     const user = await User.findOne({ email: email });
-//     if (!user) {
-//       return res.status(400).json({ success: false, message: "No user found" });
-//     }
-//     const doMatch = await bcrypt.compare(password, user.password);
-//     if (doMatch) {
-//       req.session.isLoggedIn = true;
-//       req.session.userId = user.id;
-//       await req.session.save();
-//       console.log("Session saved successfully", req.session);
-//       res.json({ success: true, message: "Logged in successfully" });
-//     } else {
-//       res.status(401).json({ success: false, message: "Wrong password" });
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     res.json({ success: false, message: err.toString() });
-//   }
-// };
-
-// exports.postSignIn = async (req, res, next) => {
-//   const { email, password, name: userName } = req.body;
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     console.log(errors.array());
-//     return res
-//       .status(422)
-//       .json({ success: false, message: errors.array()[0].msg });
-//   }
-//   try {
-//     let user = await User.findOne({ email: email });
-//     if (user) {
-//       return res
-//         .status(400)
-//         .json({ success: false, message: "Email already exists" });
-//     }
-//     const hashPassword = await bcrypt.hash(password, 12);
-//     user = new User({
-//       userName,
-//       email,
-//       password: hashPassword,
-//       cart: { items: [] },
-//     });
-//     req.session.isLoggedIn = true;
-//     req.session.userId = user.id;
-//     await req.session.save();
-//     await user.save();
-//     res.status(200).json({ success: true, message: "User Created" });
-//   } catch (err) {
-//     console.log(err);
-//     res.json({ success: false, message: err.toString() });
-//   }
-// };
-
-// exports.getLogout = (req, res, next) => {
-//   if (req.session) {
-//     req.session.destroy((err) => {
-//       if (err) {
-//         res.json({ success: false, message: err.toString() });
-//       } else {
-//         res.json({ success: true, message: "Logged out successfully" });
-//       }
-//     });
-//   }
-// };
-
-// exports.likeProduct = async (req, res, next) => {
-//   try {
-//     const { productId, like } = req.body;
-//     const update = like
-//       ? { $addToSet: { likedProducts: productId } }
-//       : { $pull: { likedProducts: productId } };
-//     const user = await User.findByIdAndUpdate(req.session.userId, update, {
-//       new: true,
-//     });
-//     await user.save();
-//     res.json({ success: true, user });
-//   } catch (err) {
-//     res.json({ success: false, message: err.toString() });
-//   }
-// };
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
@@ -164,7 +11,7 @@ exports.getLogin = (req, res, next) => {
   res.json({ success: false, message: "No session found" });
 };
 
-exports.getUser = (req, res, next) => {
+exports.getUser = (req, res, next) => {//very important part not to include req and res object in response object
   if (req.session && req.session.userId) {
     User.findById(req.session.userId)
       .then((user) => {
@@ -180,14 +27,25 @@ exports.getUser = (req, res, next) => {
             },
           });
         }
-        res.status(404).json({ success: false, message: "User not found" });
+        res.status(404).json({
+          success: false,
+          message: "User not found",
+          session: req.session ? { userId: req.session.userId } : null,
+        });
       })
       .catch((err) => {
-        console.log(err);
-        res.status(500).json({ success: false, message: "Internal Server Error" });
+        console.error("Error fetching user:", err);
+        res.status(500).json({
+          success: false,
+          message: "Internal Server Error",
+        });
       });
   } else {
-    res.status(401).json({ success: false, message: "Unauthorized access" });
+    res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+      session: req.session ? { userId: req.session.userId } : null,
+    });
   }
 };
 
